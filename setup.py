60b7b9d797a6d3187af9540f6fda2d69a2a10eba
hugomes14 <hugo.gomes1415@gmail.com>
2024-11-20 15:19:49 +0000

    configuration of the setup.py file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name= "video-crop",
+    version= "0.1.0",
+    author= "Hugo Gomes",
+    author_email= "hugo.gomes1415@gmail.com",
+    packages= find_packages("src")
+)
