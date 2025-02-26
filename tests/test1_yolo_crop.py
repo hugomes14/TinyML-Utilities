eb461749b24b023a9100f1ad8d7a2165150b7500
hugomes14 <hugo.gomes1415@gmail.com>
2025-02-26 10:39:49 +0000

    yolo dataset building script3

--- tests/test1_yolo_crop.py
+++ tests/test1_yolo_crop.py
@@ -7,9 +7,10 @@ from src.video_utils import VideoCropYolo
 VIDEO_NAME= "teste3.avi"
 DIR = os.path.dirname(os.path.abspath(__file__))
 DATASET = "yolo_dataset"
+FILE_NUMBER = 2
 
 
 
-video = VideoCropYolo(video_name= VIDEO_NAME, dir= DIR, dataset= DATASET)
+video = VideoCropYolo(video_name= VIDEO_NAME, dir= DIR, dataset= DATASET, file_number= FILE_NUMBER)
 
 video.dataset_constructer()
