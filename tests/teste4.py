167fb3170125ff5030606b2328aaef6ab70789f2
hugomes14 <hugo.gomes1415@gmail.com>
2025-03-14 11:42:31 +0000

    add .env variables

--- tests/teste4.py
+++ tests/teste4.py
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-from src.b6_smart_camera import SmartCamera
-
-camera = SmartCamera(file_name="")
-=======
 from src.b6_smart_camera import SmartCamera
 
 camera = SmartCamera(file_name="caixa_test1")
->>>>>>> 19fc62c (qq)
 camera.connection()
